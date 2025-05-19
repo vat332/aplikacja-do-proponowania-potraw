d1fa6120b3b30cb9c4d968e9c76c11946fa7547d
vat332 <oporowymakus21@gmail.com>
2025-05-19 16:12:34 +0200

    Add ingredient submission form and display list of ingredients

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,35 @@
 const Main = () => {
+  const ingredients = [
+    "Kurczak",
+    "Cebula",
+    "Masło",
+    "Czosnek",
+    "Papryka",
+    "Sól",
+  ];
+  const ingredientListItems = ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const newIngredient = formData.get("ingredient");
+    console.log(newIngredient);
+    ingredients.push(newIngredient);
+  };
   return (
     <main>
-      <form className="add-ingredient-form">
-        <input type="text" placeholder="masło" aria-label="Dodaj składnik" />
+      <form className="add-ingredient-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="masło"
+          aria-label="Dodaj składnik"
+          name="ingredient"
+        />
         <button type="submit">Dodaj składnik</button>
       </form>
+      <ul>{ingredientListItems}</ul>
     </main>
   );
 };
