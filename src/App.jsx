5bf61e5f20cc8bb6699a78a0da60bf5284a9eeff
vat332 <oporowymakus21@gmail.com>
2025-05-19 15:59:48 +0200

    Add Main component and update App structure with form for adding ingredients

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Main />
     </div>
   );
 };
---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+const Main = () => {
+  return (
+    <main>
+      <form className="add-ingredient-form">
+        <input type="text" placeholder="masło" aria-label="Dodaj składnik" />
+        <button type="submit">Dodaj składnik</button>
+      </form>
+    </main>
+  );
+};
+
+export default Main;
