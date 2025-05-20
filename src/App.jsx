6afb815420769a9421878125f1a23a71b6458086
vat332 <oporowymakus21@gmail.com>
2025-05-20 15:02:49 +0200

    Update dark background color in App component for improved contrast

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Main from "./components/Main";
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-[#f3f2f2] text-gray-900 dark:bg-[#272d53] dark:text-gray-200">
+    <div className="min-h-screen bg-[#f3f2f2] text-gray-900 dark:bg-[#323a6b] dark:text-gray-200">
       <Header />
       <Main />
     </div>
