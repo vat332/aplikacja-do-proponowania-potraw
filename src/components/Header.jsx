54d1f3b3038c591ca49c80b0995c7825522e29ce
vat332 <oporowymakus21@gmail.com>
2025-05-20 14:56:02 +0200

    - Added @svgr/webpack, prettier, and prettier-plugin-tailwindcss to devDependencies in package.json. - Replaced logo image in Header component with light and dark SVG logos for better theming support. - Removed old logo.png and added new logo.svg and logo-white.svg files for improved scalability and quality.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,17 @@
-import Logo from "../public/images/logo.png";
+import LogoLight from "../public/images/logo-white.svg";
+import LogoDark from "../public/images/logo.svg";
+
 const Header = () => {
   return (
-    <header className="flex items-center justify-center gap-3 shadow-md shadow-gray-100 pb-2 bg-[#FAFAFA]">
-      <img src={Logo} alt="logo_icon" className="w-12" />
-      <h1 className="bg-gradient-to-r from-[#dd6d48] to-[#eea992] bg-clip-text text-transparent">
+    <header className="flex items-center justify-center gap-3 bg-[#FAFAFA] pb-2 shadow-lg shadow-gray-100 dark:bg-[#272d53] dark:shadow-gray-800">
+      <img src={LogoDark} alt="logo_icon" className="w-12 dark:hidden" />
+      <img
+        src={LogoLight}
+        alt="logo_icon_dark"
+        className="hidden w-12 dark:block"
+      />
+
+      <h1 className="bg-gradient-to-r from-[#dd6d48] to-[#eea992] bg-clip-text text-transparent dark:bg-gradient-to-r dark:from-[#9507ee] dark:to-[#b5b1e7]">
         Chef Claude
       </h1>
     </header>
